0fb1a9002337c3e3ea503de817598efe3ee04eeb
hkantare <hkantare@in.ibm.com>
2021-12-02 19:51:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,41 @@ provider "ibm" {}
 }*/
 
 
-data "ibm_container_cluster_config" "cluster_config" {
+/*data "ibm_container_cluster_config" "cluster_config" {
   cluster_name_id = "bs79g2k20piq1r143ps0"
 }
 
 resource "random_id" "name" {
   byte_length = 4
+}*/
+  
+data "ibm_container_cluster_config" "testacc_ds_cluster" {
+  cluster_name_id = "c2mftm7d02sqmqa0nf10"
+  config_dir      = "."
 }
+variable "kube_config_path" {
+  default = "/tmp"
+}
+
+resource null_resource calico {
+  provisioner local-exec {
+    environment = {
+      KUBECONFIG   = data.ibm_container_cluster_config.testacc_ds_cluster.config_file_path
+    }
+    command = <<BASH
+export KUBECONFIG=$KUBECONFIG
+echo "Run Kubectl commands"
+ kubectl get pods
+        BASH
+  }
+}
+terraform {
+  required_providers {
+    ibm = {
+      version = "1.36.0"
+      source = "IBM-Cloud/ibm"
+    }
+  }
+}
+
+
